e5c1a17bcc7b94cc78c85547ad26931e8a90fa1c
Patrick Bösch <boeschpa@ethz.ch>
2023-11-13 18:50:49 +0100

    delete old stuff

--- denseLIF/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd $(dirname $0)
-genn-buildmodel.sh -c denseLIFModel.cc;
-genn-create-user-project.sh denseLIF denseLIFSimulation.cc;
-make;
-./denseLIF;
-python3 ../util/plot_output.py denseLIF_output.V.dat;
