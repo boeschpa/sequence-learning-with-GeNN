9849ba2dbeda6e1d2dfcc69ab86746d2c6119494
Patrick Bösch <boeschpa@ethz.ch>
2023-12-07 16:24:33 +0100

    dots

--- hypercolumn_BCPNN_fast/plot_spikes_hypercolumn.py
+++ hypercolumn_BCPNN_fast/plot_spikes_hypercolumn.py
@@ -90,9 +90,9 @@ fig.set_size_inches(16,16)
 # plot spikes
 if (sim_time != time[-1] and time_start != 0):
     spike_id = np.where(np.logical_and(time<sim_time, time > time_start))[0]
-    ax[0].plot(time[spike_id], spikes[spike_id],".")
+    ax[0].plot(time[spike_id], spikes[spike_id],".", markersize=1)
 else:
-    ax[0].plot(time, spikes,".")
+    ax[0].plot(time, spikes,".", markersize=1)
 
 for i in range(param.hyper_height*param.hyper_width-1):
     ax[0].axhline(y = (i+1)*param.N_pyramidal*param.N_minicolumns, color = 'k', linestyle = '-') 
