5a75c743004cadde8dcd2db9ab86e7e80b884ec2
Patrick Bösch <boeschpa@ethz.ch>
2023-12-14 17:01:45 +0100

    compute accuracy wip

--- hypercolumn_BCPNN_fast/model_param.h
+++ hypercolumn_BCPNN_fast/model_param.h
@@ -3,12 +3,12 @@
 // only use constants (for generate_code_interface to work)
 
 // model params
-const float time_step = 0.1;
+const float time_step = 0.05;
 const int N_minicolumns = 10;
 const int N_basket = 30;
-const int N_pyramidal = 10;
+const int N_pyramidal = 30;
 const float wta_prob = 0.7;
-const float lateral_prob = 0.25;
+const float lateral_prob = 0.5;
 const int hyper_width = 2;
 const int hyper_height = 1;
 const float fMax = 50.0;
@@ -20,8 +20,8 @@ const float wta_ampa_conductance = 0.00602; // 1.22 nS
 const float biasBaseGain = 0.05; // 50 pA
 
 // simulation params
-const float recall_time = 50.0;
-const float recall_break = 10.0;
+const float recall_time = 1000.0;
+const float recall_break = 0.0;
 const float background_freq = 100.0;
 
 // training params
@@ -29,7 +29,7 @@ const float training_freq = 150.0;
 const int N_patterns = 5;
 const float pattern_time = 200.0; // (ms) 10 attr/s
 const float pattern_break = 0.0;  // (ms) 0 for sequence learning
-const int epochs = 1;             // number of training passes
+const int epochs = 5;             // number of training passes
 
 const std::string minicolumn_basename = "M";
 const std::string baskets_name = "_baskets";
