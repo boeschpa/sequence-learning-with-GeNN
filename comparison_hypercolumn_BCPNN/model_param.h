df4299de3fae93c0d1bbc0667ed1396c3e568c28
Patrick Bösch <boeschpa@ethz.ch>
2023-12-28 21:15:04 +0100

    model params comparison with recall

--- comparison_hypercolumn_BCPNN/model_param.h
+++ comparison_hypercolumn_BCPNN/model_param.h
@@ -9,11 +9,11 @@ const int N_basket = 24;
 const int N_pyramidal = 30;
 const float wta_prob = 0.7;
 const float lateral_prob = 0.20;
-const int hyper_width = 2;
-const int hyper_height = 2;
+const int hyper_width = 4;
+const int hyper_height = 4;
 const float fMax = 25.0;
 const float fDesired = 1.0;
-const float epsilon = 0.0001;
+const float epsilon = 0.001;
 const float lateral_ampa_conductance = 0.0060; // 6.02 nS
 const float lateral_nmda_conductance = 0.0010; // 1.22 nS
 const float wta_ampa_conductance = 0.0010; // 6.02 nS
@@ -21,8 +21,8 @@ const float wta_gaba_conductance = 0.0090; // 6.02 nS
 const float biasBaseGain = 0.05; // 50 pA
 
 // simulation params
-const float recall_time = 500.0;
-const float settle_time = 500.0;
+const float recall_time = 1000.0;
+const float settle_time = 1000.0;
 const float background_freq = 17.0;
 
 // training params
